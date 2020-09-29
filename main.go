c3186ded9a4b32d7bbd6f282fa938519a2504113
Martel <martel.mosqueda@alumnos.udg.mx>
2020-09-29 12:10:16 -0500

    numero e

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+// Returns the facturial of a positive number
+func factorial(number int) int {
+	if number == 0 {
+		return 1
+	}
+	return number * factorial(number-1)
+}
+
+func main() {
+	// Store result
+	var e float64
+	// Iterations
+	var totalIterations int
+
+	// ask for iterations
+	fmt.Scanf("%d ", &totalIterations)
+
+	for i := 0; i < totalIterations; i++ {
+		e += 1 / float64(factorial(i))
+	}
+
+	fmt.Print(e)
 }
